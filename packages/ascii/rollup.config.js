2693df55b8aa3c61af73c0609f4e9deeb54763c1
GSDV <sdvgab@gmail.com>
2025-12-15 10:37:09 -0600

    Rollup

---
+++ packages/ascii/rollup.config.js
@@ -0,0 +1,46 @@
+import typescript from '@rollup/plugin-typescript';
+import copy from '@rollup/plugin-copy';
+
+import packageJson from './package.json' assert { type: 'json' };
+
+
+
+export default [
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: packageJson.main,
+                format: 'cjs',
+                exports: 'named',
+                sourcemap: true
+            },
+            {
+                file: packageJson.module,
+                format: 'esm',
+                sourcemap: true
+            }
+        ],
+        plugins: [
+            typescript({
+                tsconfig: './tsconfig.json',
+                declaration: true,
+                declarationMap: true,
+                declarationDir: 'lib'
+            }),
+            copy({
+                targets: [
+                    { 
+                        src: 'src/assets/fonts/*.ttf', 
+                        dest: 'lib' 
+                    }
+                ]
+            })
+        ],
+        external: [
+            'react',
+            'react-native',
+            '@shopify/react-native-skia'
+        ]
+    }
+];
