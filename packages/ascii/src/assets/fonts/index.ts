86fba7f284164a20fa84bd05efa21b7cf54a861c
GSDV <sdvgab@gmail.com>
2025-12-18 12:27:47 -0600

    charAspectRatio and lineHeightMultiplier

--- packages/ascii/src/assets/fonts/index.ts
+++ packages/ascii/src/assets/fonts/index.ts
@@ -1,8 +1,28 @@
-export const DEFAULT_FONTS = {
-    CourierPrime: [require('./CourierPrime-Regular.ttf')],
-    DejaVuSansMono: [require('./DejaVuSansMono.ttf')]
+export type Font = {
+    sources: any[];
+    charAspectRatio?: number;
+    lineHeightMultiplier?: number;
+};
+
+export type FontMap = Record<string, Font>;
+
+
+
+export const DEFAULT_FONTS: FontMap = {
+    CourierPrime: {
+        sources: [require('./CourierPrime-Regular.ttf')],
+        charAspectRatio: 0.6,
+        lineHeightMultiplier: 1
+    },
+    DejaVuSansMono: {
+        sources: [require('./DejaVuSansMono.ttf')],
+        charAspectRatio: 0.55,
+        lineHeightMultiplier: 1.1
+    }
 };
 
 
 
 export const DEFAULT_FONT = 'CourierPrime';
+export const DEFAULT_CHAR_ASPECT_RATIO = 0.6;
+export const DEFAULT_LINE_HEIGHT_MULTIPLIER = 1.0;
